9235128d02564ff186de29b6ac47f4c7e75cff79
Zhukovskiy Stanislav <menistrel2005@yandex.ru>
2017-06-28 17:15:38 +0300

    made about.html, fixed task build for _modules, add footer for different pages

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -3,7 +3,7 @@
 module.exports = function() {
   $.gulp.task('pug', function() {
     // var YOUR_LOCALS = require('./content.json');
-    return $.gulp.src(['./source/template/**/*.pug', '!./source/template/_template.pug'])
+    return $.gulp.src(['./source/template/**/*.pug', '!./source/template/**/_*.pug'])
       .pipe($.gp.pug({ 
         // locals: YOUR_LOCALS,
         pretty: true }))
