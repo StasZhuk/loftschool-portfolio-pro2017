9ac36b6c617c21ca5b74d482331c45f840543b61
Zhukovskiy Stanislav <menistrel2005@yandex.ru>
2017-07-01 17:33:07 +0300

    fixed some bugs(button, user-block), add flip auth menu on click

--- gulp/tasks/webpack.js
+++ gulp/tasks/webpack.js
@@ -2,11 +2,10 @@
 
 module.exports = function() {
   $.gulp.task('webpack', function() {
-    return $.gulp.src($.path.source + '/js/index.js')
+    return $.gulp.src($.path.source + '/js/pages/index.js')
         .pipe($.gp.webpack({
             entry: {
-                index: $.path.source + '/js/index.js',
-                auth: $.path.source + '/js/pages/auth.js',
+                index: $.path.source + '/js/pages/index.js',
                 about: $.path.source + '/js/pages/about.js',
                 works: $.path.source + '/js/pages/works.js',
                 blog: $.path.source + '/js/pages/blog.js'
--- source/js/pages/index.js
+++ source/js/pages/index.js
@@ -1,3 +1,4 @@
 const $ = require('jquery');
 
 console.log($);
+flipper();
