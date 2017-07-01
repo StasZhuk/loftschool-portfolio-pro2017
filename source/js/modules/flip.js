744b53ec3f42e722b5b6d2fc2e59599b84c102f9
Zhukovskiy Stanislav <menistrel2005@yandex.ru>
2017-07-01 18:22:06 +0300

    add flip.js, write function flip that animate click button_auth action

---
+++ source/js/modules/flip.js
@@ -0,0 +1,28 @@
+module.exports = function() {
+
+    return window.onload = function () {
+        //получаем элементы
+        var button = document.querySelector('.btn__auth');
+        var flipContainer = document.querySelector('.flip__container');
+        var body = document.querySelector('.auth');
+
+        
+        //вешаем на него событие
+        button.onclick = function(e) {
+            e.stopPropagation();
+            flipContainer.classList.toggle('flip');
+            //предотвращаем переход по ссылке href
+            return false;
+        }
+
+
+        body.onclick = function(e) {
+            e.stopPropagation();
+            if(e.target == body) {
+                flipContainer.classList.remove('flip');
+            }
+            return false;
+        }
+    }
+
+}
